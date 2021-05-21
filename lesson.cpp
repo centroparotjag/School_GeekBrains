b4a220ef6760b0322f9aa8b41d59177628aec0f1
Oleksand <centroparotjag@gmail.com>
2021-05-22 00:45:42 +0300

    minor edits

--- lesson.cpp
+++ lesson.cpp
@@ -6,29 +6,29 @@ int main(int argc, const char** argv) {
 
 //	1 .Написать программу, проверяющую что сумма двух чисел лежит в пределах 
 //	от 10 до 20 (включительно), если да – вывести true, в противном случае – false;
-	// printf("\n1 task:");
+	printf("\n1.");
 
-	// int first_num  = 0;
-	// int second_num = 0;
+	int first_num  = 0;
+	int second_num = 0;
 
-	// //------- Input of two numbers ------
-	// printf("\tInput first number : ");
-	// std::cin >> first_num;
-	// printf("\tInput second number: ");
-	// std::cin >> second_num;
+	//------- Input of two numbers ------
+	printf("\tInput first number : ");
+	std::cin >> first_num;
+	printf("\tInput second number: ");
+	std::cin >> second_num;
 
 
-	// if ( (first_num + second_num) >= 10 && (first_num + second_num) <= 20 ){
-	// 	printf("\ttrue\n");
-	// }
-	// else {
-	// 	printf("\tfalse\n");
-	// }
+	if ( (first_num + second_num) >= 10 && (first_num + second_num) <= 20 ){
+		printf("\ttrue\n");
+	}
+	else {
+		printf("\tfalse\n");
+	}
 
 
 //	2.Написать программу, проверяющую, является ли некоторое число - натуральным простым. 
 //	Простое число - это число, которое делится без остатка только на единицу и себя само.
-	printf("\n1 task:");
+	printf("\n2.");
 	unsigned long long number = 0;
 
 	//------- Input of number ------
