09cb2744d73f38cf9b4036e40e8263596d6f6c39
Oleksandr <centroparotjag@gmail.com>
2021-06-02 22:33:33 +0300

    add files homework 6

--- main.h
+++ main.h
@@ -3,18 +3,7 @@
 
 #include <iostream>
 
-namespace task1 {
-    void Task1_replacement_data_in_array1  (char *arr, char size_arr1);
-}
-
-
-void Task2_filling_the_array2 (char *arr);
-
-int Task3_checkBalance (char *array, char size_arr);
-
-void Task4_shift_array (char *array, char size_arr,  char n);
-
-void Task5_invert_va_data (int size, ...);
+void create_and_write_file_txt (const std::string& name_file ,const std::string& word );
 
 
 #endif // MAIN_H
